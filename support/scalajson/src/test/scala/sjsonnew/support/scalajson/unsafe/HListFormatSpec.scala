d51002c44644b67ecf8f028b823f13610dc8bc41
Dale Wijnand <dale.wijnand@gmail.com>
2016-12-19 15:02:45 +0000

    Introduce HListJF
    Used as an inner TC to implement JsonFormat[HList], so it can manage
    when the hlist array starts and ends.

--- support/scalajson/src/test/scala/sjsonnew/support/scalajson/unsafe/HListFormatSpec.scala
+++ support/scalajson/src/test/scala/sjsonnew/support/scalajson/unsafe/HListFormatSpec.scala
@@ -21,13 +21,17 @@ class HListFormatSpec extends FlatSpec {
   val bobJson = JArray(JString("Bob"), JNumber(23))
   val bobJsonStr = """["Bob",23]"""
 
+  it should "HNil.jsonRoundTrip correctly"                      in assertRoundTrip(HNil)
+  it should "(23 :+: HNil).jsonRoundTrip correctly"             in assertRoundTrip(23 :+: HNil)
+  it should "(23 :+: \"foo\" :+: HNil).jsonRoundTrip correctly" in assertRoundTrip(23 :+: "foo" :+: HNil)
+
   it should "Peep.toJson correctly"              in assert(bob.toJson === bobJson)
   it should "Peep.toJsonStr correctly"           in assert(bob.toJsonStr === bobJsonStr)
   it should "String.toJson correctly"            in assert(bobJsonStr.toJson === bobJson)
   it should "String.fromJsonStr[Peep] correctly" in assert(bobJsonStr.fromJsonStr[Peep] === bob)
   it should "Peep.jsonRoundTrip correctly"       in assertRoundTrip(bob)
 
-  it should "HList.jsonRoundTrip correctly" in pendingUntilFixed(assertRoundTrip(bob :+: Peep("Amy", 22) :+: HNil))
+  it should "HList.jsonRoundTrip correctly" in assertRoundTrip(bob :+: Peep("Amy", 22) :+: HNil)
 
   private def assertRoundTrip[A: JsonWriter : JsonReader](x: A) = assert(x === x.jsonRoundTrip)
 }
