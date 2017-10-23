741c0a4524ff3c293733c7e984f8dccab1385ccf
Suhas Gaddam <suhas.g.2011@gmail.com>
2017-10-23 15:06:51 -0700

    Bump to jawn-parser 0.11.0

--- support/scalajson/src/main/scala/sjsonnew/support/scalajson/unsafe/Parser.scala
+++ support/scalajson/src/main/scala/sjsonnew/support/scalajson/unsafe/Parser.scala
@@ -11,19 +11,18 @@ object Parser extends SupportParser[JValue] {
       def jnull() = JNull
       def jfalse() = JFalse
       def jtrue() = JTrue
-      def jnum(s: String) = JNumber(s)
-      def jint(s: String) = JNumber(s)
-      def jstring(s: String) = JString(s)
+      def jnum(s: CharSequence, decIndex: Int, expIndex: Int): JValue = JNumber(s.toString)
+      def jstring(s: CharSequence) = JString(s.toString)
       def singleContext() = new FContext[JValue] {
         var value: JValue = _
-        def add(s: String) = value = jstring(s)
+        def add(s: CharSequence) = value = jstring(s)
         def add(v: JValue) = value = v
         def finish: JValue = value
         def isObj: Boolean = false
       }
       def arrayContext() = new FContext[JValue] {
         private val vs = ArrayBuffer.empty[JValue]
-        def add(s: String) = vs += jstring(s)
+        def add(s: CharSequence) = vs += jstring(s)
         def add(v: JValue) = vs += v
         def finish: JValue = JArray(vs.toArray)
         def isObj: Boolean = false
@@ -32,7 +31,7 @@ object Parser extends SupportParser[JValue] {
         private var key: String = _
         private var vs = ArrayBuffer.empty[JField]
         private def andNullKey[A](t: => Unit): Unit = { t; key = null }
-        def add(s: String) = if (key == null) key = s else andNullKey(vs += JField(key, jstring(s)))
+        def add(s: CharSequence) = if (key == null) key = s.toString else andNullKey(vs += JField(key, jstring(s)))
         def add(v: JValue) = andNullKey(vs += JField(key, v))
         def finish: JValue = JObject(vs.toArray)
         def isObj: Boolean = true
