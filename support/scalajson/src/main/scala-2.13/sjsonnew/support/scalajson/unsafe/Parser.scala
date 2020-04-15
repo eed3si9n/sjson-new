d18aecd6c1719000e7585f12912634d1c1afe4bc
Seth Tisue <seth@tisue.net>
2020-04-15 13:48:24 -0700

    on Scala 2.13, upgrade to jawn 1.0

--- support/scalajson/src/main/scala-2.13/sjsonnew/support/scalajson/unsafe/Parser.scala
+++ support/scalajson/src/main/scala-2.13/sjsonnew/support/scalajson/unsafe/Parser.scala
@@ -8,36 +8,36 @@ import org.typelevel.jawn.{ Facade, FContext, SupportParser }
 object Parser extends SupportParser[JValue] {
   implicit val facade: Facade[JValue] =
     new Facade[JValue] {
-      def jnull() = JNull
-      def jfalse() = JFalse
-      def jtrue() = JTrue
-      def jnum(s: CharSequence, decIndex: Int, expIndex: Int) = JNumber(s.toString)
+      def jnull(index: Int) = JNull
+      def jfalse(index: Int) = JFalse
+      def jtrue(index: Int) = JTrue
+      def jnum(s: CharSequence, decIndex: Int, expIndex: Int, index: Int) = JNumber(s.toString)
       def jint(s: String) = JNumber(s)
-      def jstring(s: CharSequence) = JString(s.toString)
-      def singleContext() = new FContext[JValue] {
+      def jstring(s: CharSequence, index: Int) = JString(s.toString)
+      def singleContext(index: Int) = new FContext[JValue] {
         var value: JValue = _
-        def add(s: CharSequence) = value = jstring(s)
-        def add(v: JValue) = value = v
-        def finish: JValue = value
+        def add(s: CharSequence, index: Int) = value = jstring(s, index)
+        def add(v: JValue, index: Int) = value = v
+        def finish(index: Int): JValue = value
         def isObj: Boolean = false
       }
-      def arrayContext() = new FContext[JValue] {
+      def arrayContext(index: Int) = new FContext[JValue] {
         private val vs = ArrayBuffer.empty[JValue]
-        def add(s: CharSequence) = vs += jstring(s)
-        def add(v: JValue) = vs += v
-        def finish: JValue = JArray(vs.toArray)
+        def add(s: CharSequence, index: Int) = vs += jstring(s, index)
+        def add(v: JValue, index: Int) = vs += v
+        def finish(index: Int): JValue = JArray(vs.toArray)
         def isObj: Boolean = false
       }
-      def objectContext() = new FContext[JValue] {
+      def objectContext(index: Int) = new FContext[JValue] {
         private var key: String = _
         private var vs = ArrayBuffer.empty[JField]
         private def andNullKey[A](t: => Unit): Unit = { t; key = null }
-        def add(s: CharSequence) = {
+        def add(s: CharSequence, index: Int) = {
           if (key == null) key = s.toString
-          else andNullKey(vs += JField(key, jstring(s)))
+          else andNullKey(vs += JField(key, jstring(s, index)))
         }
-        def add(v: JValue) = andNullKey(vs += JField(key, v))
-        def finish: JValue = JObject(vs.toArray)
+        def add(v: JValue, index: Int) = andNullKey(vs += JField(key, v))
+        def finish(index: Int): JValue = JObject(vs.toArray)
         def isObj: Boolean = true
       }
     }
